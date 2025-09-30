c72145cc77477f6e9ba4cd10df2c10136214563d
WoutVanImpe <145569527+WoutVanImpe@users.noreply.github.com>
2025-09-30 16:06:02 +0200

    basic todo app done (checking todo's, deleting todo's and adding todo's)

--- src/app/pages/todo/todo.ts
+++ src/app/pages/todo/todo.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { TodoItem } from './components/todo-item';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'todopage',
   template: `<h1>Todos</h1>
+    @if(!addingTodoState){
     <div class="todo-container">
       @for (todo of todos; track todo.id){
       <todoItem
@@ -15,8 +17,21 @@ import { TodoItem } from './components/todo-item';
       />
       }
     </div>
-    <p>{{ completedCount }} van de {{ todos.length }} todos gedaan!</p>`,
-  imports: [TodoItem],
+    <p>{{ completedCount }} van de {{ todos.length }} todos gedaan!</p>
+    } @else {
+    <form [formGroup]="todoForm" (ngSubmit)="handleSubmit()">
+      <label>Title: <input id="title" formControlName="title" type="text" /></label>
+      <input id="submit" [disabled]="!todoForm.valid" type="submit" />
+    </form>
+    }
+    <button (click)="changeState()" class="stateButton">
+      @if (addingTodoState){
+      <p>cancel</p>
+      } @else {
+      <p>add todo</p>
+      }
+    </button>`,
+  imports: [TodoItem, ReactiveFormsModule],
   styleUrls: ['./todo.module.scss'],
 })
 export class TodoPage {
@@ -26,9 +41,17 @@ export class TodoPage {
     { id: 3, title: 'koken', checked: false },
   ];
 
+  idCount = this.todos.length + 1;
+
+  addingTodoState = false;
+
   get completedCount() {
     return this.todos.filter((t) => t.checked).length;
   }
+
+  todoForm = new FormGroup({
+    title: new FormControl('', Validators.required),
+  });
 
   completeTodo(event: { id: number; checked: boolean }) {
     this.todos = this.todos.map((t) => (t.id === event.id ? { ...t, checked: event.checked } : t));
@@ -36,5 +59,17 @@ export class TodoPage {
 
   deleteTodo(event: { id: number }) {
     this.todos = this.todos.filter((t) => t.id != event.id);
+  }
+
+  handleSubmit() {
+    const newTodo = { id: this.idCount, title: this.todoForm.value.title!, checked: false };
+    this.todos.push(newTodo);
+    this.idCount++;
+    this.addingTodoState = false;
+    this.todoForm.reset();
+  }
+
+  changeState() {
+    this.addingTodoState = !this.addingTodoState;
   }
 }
