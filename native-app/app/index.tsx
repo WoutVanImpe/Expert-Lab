f9ea33f0d1b6f948b251a7b377e87f3d0e969183
WoutVanImpe <145569527+WoutVanImpe@users.noreply.github.com>
2025-11-25 10:02:56 +0100

    inserted new styled components

--- native-app/app/index.tsx
+++ native-app/app/index.tsx
@@ -1,14 +1,28 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet } from "react-native";
+import React from "react";
+import ThemedView from "../components/ThemedView";
+import ThemedLink from "../components/ThemedLink";
+import ThemedText from "../components/ThemedText";
 
 const Home = () => {
-  return (
-    <View>
-      <Text>Home</Text>
-    </View>
-  )
-}
+	return (
+		<ThemedView style={styles.container}>
+			<ThemedText title style={styles.title}>Home</ThemedText>
+			<ThemedLink href="/about">About</ThemedLink>
+		</ThemedView>
+	);
+};
 
-export default Home
+export default Home;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	title: {
+		fontWeight: "bold",
+		fontSize: 20,
+	},
+});
